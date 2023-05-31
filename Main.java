2473d10bc6af3c7cb41cea8398aa8214dc261f35
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2023-05-31 10:24:15 -0300

    Enunciado

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
+
+/*
+1 - Criar uma Aplicação Bancária com funcionalidades:
+    1.A - Criar conta Pessoa Física e Pessoa Júridica
+    1.B - Permitir busca de conta pelo n° da conta
+    1.C - Instanciar saldo
+    1.D - Criar método saque
+    1.E - Criar métodos depósito
+    1.F - Criar método empréstimo
+*/
