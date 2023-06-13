fd80123aed6d38f9aab919c4ec4ab99a5c9d049c
Lorenzo O. Zimbres <97912499+LoriaLawrenceZ@users.noreply.github.com>
2023-06-13 16:33:02 +0000

    tirando comentarios

--- Main.java
+++ Main.java
@@ -33,13 +33,3 @@ public class Main {
         sc.close();
     }
 }
-
-/*
-1 - Criar uma Aplicação Bancária com funcionalidades:
-    1.A - Criar conta Pessoa Física e Pessoa Júridica
-    1.B - Permitir busca de conta pelo n° da conta
-    1.C - Instanciar saldo
-    1.D - Criar método saque
-    1.E - Criar métodos depósito
-    1.F - Criar método empréstimo
-*/
